e9bfc120ddeabb23f6cbc118f56e1febaf1b551b
grayorc <grayorc@proton.me>
2025-02-28 14:53:38 +0330

    fix inline mode improve search fix image

--- src/bot.ts
+++ src/bot.ts
@@ -7,20 +7,21 @@ interface CSVRow {
   name: string;
   rb_br: string;
   nation: string;
+  is_premium: string;
 }
 
-const flags_emoji: { [key: string ]: string } = {
-  "britain":"🇬🇧",
-  "china":"🇨🇳",
-  "france":"🇫🇷",
-  "germany":"🇩🇪",
-  "israel":"🇮🇱",
-  "italy":"🇮🇹",
-  "japan":"🇯🇵",
-  "sweden":"🇸🇪",
-  "usa":"🇺🇸",
-  "ussr":"🇷🇺"
-}
+const emoji: { [key: string]: string } = {
+  "britain": "🇬🇧",
+  "china": "🇨🇳",
+  "france": "🇫🇷",
+  "germany": "🇩🇪",
+  "israel": "🇮🇱",
+  "italy": "🇮🇹",
+  "japan": "🇯🇵",
+  "sweden": "🇸🇪",
+  "usa": "🇺🇸",
+  "ussr": "🇷🇺"
+};
 
 const vehicles: CSVRow[] = [];
 fs.createReadStream('data/data.csv')
@@ -38,27 +39,43 @@ const bot = new TelegramBot(token, { polling: true });
 let find_status: { [key: number]: boolean } = {};
 let search_status: { [key: number]: boolean } = {};
 
-bot.on('inline_query', (query) => {
+function image(vehicle: string): string {
+  return `https://natgo.xyz/files/wt/images/garageimages/${vehicle}.jpg`;
+}
+
+function findArrayByName(name: string): CSVRow[] {
+  let search = vehicles.filter(vehicle => vehicle.name.toLowerCase().replace("_", "").includes(name));
+  console.log(search.length);
+  if (search.length > 0)
+    return search;
+  else
+    return vehicles.filter(vehicle => vehicle.name.toLowerCase().includes(name));
+}
+
+bot.on('inline_query', async (query) => {
   const queryText = query.query.trim().toLowerCase();
-  const searchResult = findArrayByName(queryText);
+  const searchResult = findArrayByName(queryText).slice(0, 10); // Limit results to 10
 
   const results: TelegramBot.InlineQueryResultArticle[] = searchResult.map((vehicle, index) => ({
     type: 'article',
     id: String(index),
     title: vehicle.name,
-    description: `RB BR: ${vehicle.rb_br} | Nation: ${flags_emoji[vehicle.nation.toLowerCase()]}`,
+    description: `RB BR: ${vehicle.rb_br} | Nation: ${emoji[vehicle.nation.toLowerCase()]}`,
+    thumb_url: image(vehicle.name),
     input_message_content: {
-      message_text: vehicle_info_tpl(vehicle.name, vehicle.nation.toLowerCase(), vehicle.rb_br, vehicle.rb_br)
+      message_text: inline_vehicle_info_tpl(vehicle.name, vehicle.nation.toLowerCase(), vehicle.rb_br, vehicle.rb_br, vehicle.is_premium),
+      parse_mode: 'Markdown'
     }
   }));
 
-  bot.answerInlineQuery(query.id, results);
+  await bot.answerInlineQuery(query.id, results);
 });
 
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
   find_status[chatId] = false;
-  bot.sendMessage(chatId, "Hello world!");
+  console.log(msg.chat.username)
+  bot.sendMessage(chatId, "Hi!");
 });
 
 bot.onText(/\/find/, (msg) => {
@@ -67,65 +84,69 @@ bot.onText(/\/find/, (msg) => {
   bot.sendMessage(chatId, "What vehicle are you looking for? \n hint: `a_10c`", { parse_mode: 'Markdown' });
 });
 
-bot.on("message", (msg) => {
+bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
   const text = msg.text;
-
   if (find_status[chatId] === true && text) {
     const searchResult = findArrayByName(text.trim().toLowerCase());
     console.log(searchResult.length);
-    if (searchResult) {
-        find_status[chatId] = true;
-        if(searchResult.length > 1){
-          let message:string = "Choose your vehicle from the list:\n"
-            searchResult.forEach(element => {
-              message += `/${element.name}\n`;
-          });
+    if (searchResult.length > 0) {
+      if (searchResult.length > 1) {
+        let message: string = "Choose your vehicle from the list:\n";
+        searchResult.forEach(element => {
+          message += `/${element.name}\n`;
+        });
         search_status[chatId] = true;
         bot.sendMessage(chatId, message);
-        }else{
-          let imageUrl = `https://natgo.xyz/files/wt/images/garageimages/${searchResult[0].name}.jpg`;
-          bot.sendPhoto(chatId,imageUrl,{ caption: vehicle_info_tpl(searchResult[0].name, searchResult[0].nation.toLocaleLowerCase(), searchResult[0].rb_br,searchResult[0].rb_br) })
-          // bot.sendMessage(chatId, vehicle_info_tpl(searchResult[0].name, searchResult[0].nation.toLocaleLowerCase(), searchResult[0].rb_br,searchResult[0].rb_br));
-        }
-      find_status[chatId] = false;
+      } else {
+        bot.sendPhoto(chatId, image(searchResult[0].name), {
+          caption: vehicle_info_tpl(searchResult[0].name, searchResult[0].nation.toLowerCase(), searchResult[0].rb_br, searchResult[0].rb_br,searchResult[0].is_premium)
+        });
+      }
     } else {
       bot.sendMessage(chatId, "Vehicle not found. Please try again.");
     }
-  }else if (search_status[chatId] === true && text) { 
-    let result = findByName(text.slice(1));
+    find_status[chatId] = false;
+  } else if (search_status[chatId] === true && text) {
+    let result = findByName(text.slice(1).toLowerCase());
     console.log(text);
     console.log(result);
     if (result) {
-      let imageUrl = `https://natgo.xyz/files/wt/images/garageimages/${result.name}.jpg`;
-      bot.sendPhoto(
-        chatId, 
-        imageUrl ,{ 
-        caption : vehicle_info_tpl(result.name, result.nation.toLocaleLowerCase(), result.rb_br,result.rb_br)
+      bot.sendPhoto(chatId, image(result.name), {
+        caption: vehicle_info_tpl(result.name, result.nation.toLowerCase(), result.rb_br, result.rb_br,result.is_premium)
       });
-      bot.sendMessage(chatId, vehicle_info_tpl(result.name, result.nation.toLocaleLowerCase(), result.rb_br,result.rb_br));
     }
     search_status[chatId] = false;
   }
-  
 });
-
-function findArrayByName(name: string): CSVRow[] {
-  return vehicles.filter(vehicle => vehicle.name.toLowerCase().includes(name));
-}
 
 function findByName(name: string): CSVRow | undefined {
   return vehicles.find(vehicle => vehicle.name.toLowerCase() === name);
 }
 
-function vehicle_info_tpl(name: string, nation: string, rb_br: string, ab_br: string): string {
-  console.log(flags_emoji[nation])
+function vehicle_info_tpl(name: string, nation: string, rb_br: string, ab_br: string,premium: string): string {
+  let pr_emoji = premium === "TRUE"?`✅`:`❌`;
   let message: string = `
+  Found vehicle:
+  Name: ${name}
+  Nation: ${emoji[nation]}
+  RB BR: ${rb_br}
+  AB BR: ${ab_br}
+  Premium: ${pr_emoji}`
+  ;
+  return message;
+}
+
+function inline_vehicle_info_tpl(name: string, nation: string, rb_br: string, ab_br: string,premium: string): string {
+  let pr_emoji = premium === "TRUE"?`✅`:`❌`;
+  const imageUrl = image(name);
+  return `
   Found vehicle:
   Name: ${name}
   RB BR: ${rb_br}
   AB BR: ${ab_br}
-  Nation: ${flags_emoji[nation]}
+  Nation: ${emoji[nation]}
+  Premium: ${pr_emoji}
+  [Vehicle Image](${imageUrl})
   `;
-  return message;
 }
